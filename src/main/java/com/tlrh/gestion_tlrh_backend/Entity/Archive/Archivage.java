be958be04a969267b8c72667177586c43c7cf480
Shadowx9920 <youssefoutahar1@gmail.com>
2023-07-10 12:10:39 +0100

    mutlple inheritance classes

--- src/main/java/com/tlrh/gestion_tlrh_backend/Entity/Archive/Archivage.java
+++ src/main/java/com/tlrh/gestion_tlrh_backend/Entity/Archive/Archivage.java
@@ -6,10 +6,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import lombok.Data;
 
 @Entity
 @Data
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Archivage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
