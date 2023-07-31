c51e76273a9e3c81d4395d3e99e08c7466341463
OumniKah <oumnialk27@gmail.com>
2023-07-31 15:54:43 +0100

    URL problem is solved

--- src/main/java/com/tlrh/gestion_tlrh_backend/controllers/CollaborateurController.java
+++ src/main/java/com/tlrh/gestion_tlrh_backend/controllers/CollaborateurController.java
@@ -208,7 +208,7 @@ public class CollaborateurController {
          return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    @GetMapping("/get/NonManager")
+    @GetMapping("/get/NonManagers")
     public ResponseEntity<List<Collaborateur>> GetNonManagers(){
         try {
             return new ResponseEntity<>(collaborateurService.getNonManagerRH(),HttpStatus.OK);
@@ -217,6 +217,7 @@ public class CollaborateurController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
 
 
 
