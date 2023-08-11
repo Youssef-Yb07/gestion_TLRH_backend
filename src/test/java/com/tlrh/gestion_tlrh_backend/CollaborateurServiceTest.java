7749c81fb93ae3358c6254889e99abb6d2608c6c
amina elharchaoui <amina.elharchaoui11@gmail.com>
2023-08-11 15:33:55 +0200

    update Male&FemalRatio

--- src/test/java/com/tlrh/gestion_tlrh_backend/CollaborateurServiceTest.java
+++ src/test/java/com/tlrh/gestion_tlrh_backend/CollaborateurServiceTest.java
@@ -6,11 +6,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import com.tlrh.gestion_tlrh_backend.dto.CollaborateurDto;
 import com.tlrh.gestion_tlrh_backend.entity.Collaborateur;
 import com.tlrh.gestion_tlrh_backend.repositories.CollaborateurRepository;
 import com.tlrh.gestion_tlrh_backend.service.CollaborateurService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class CollaborateurServiceTest {
     @Mock
@@ -37,13 +43,18 @@ public class CollaborateurServiceTest {
         // managerRH.getRoles().add(new Role(managerMatricule, "Manager RH", );  //TODO fix this
 
         // Mocking collaborateurRepository
-        Mockito.when(collaborateurRepository.findById(collaborateurMatricule))
+        when(collaborateurRepository.findById(collaborateurMatricule))
                .thenReturn(Optional.of(collaborateur));
-        Mockito.when(collaborateurRepository.findById(managerMatricule))
+        when(collaborateurRepository.findById(managerMatricule))
                .thenReturn(Optional.of(managerRH));
 
         //CollaborateurDto resultDto = collaborateurService.assignCollaborateurToManager(collaborateurMatricule, managerMatricule);
 
         //Assertions.assertEquals(managerMatricule, resultDto.getManagerRH());
     }
+
+
+
 }
+
+
